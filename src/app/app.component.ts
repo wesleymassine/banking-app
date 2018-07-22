6ca4c2cfc687d2365d501ef00f7b3a844d001ff1
Danilo Augostinho <danilo.dasilva@zup.com.br>
2018-07-22 16:19:05 -0300

    arquivos iniciais do projeto criado para a série de tutoriais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AppComponent, {
+      height: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
 }
